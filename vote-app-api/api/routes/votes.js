3887f135d9d6dd90e991391a3635eb8c36cd9f6f
slowdata <slowmotion@outlook.com>
2018-01-08 00:57:26 +0000

    Vote methods with static content

--- vote-app-api/api/routes/votes.js
+++ vote-app-api/api/routes/votes.js
@@ -6,10 +6,40 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ message: 'Here will be a list of votes' })
 })
 
-router.get('/voteId', (req, res, next) => {
+router.get('/:voteId', (req, res, next) => {
   const id = req.params.voteId
 
   res.status(200).json({ message: `This is you vote with ID: ${id}` })
+})
+
+router.post('/', (req, res, next) => {
+  const vote = {
+    _id: req.body.voteId,
+    topic: req.body.topic,
+    description: req.body.description
+  }
+
+  console.log(vote)
+  res.status('201').json({
+    message: "Vote as been created",
+    vote: vote
+  })
+})
+
+router.patch('/:voteId', (req, res, next) => {
+  const id = req.params.voteId
+
+  res.status(200).json({
+    message: `Successfully updated vote with ID: ${id}`
+  })
+})
+
+router.delete('/:voteId', (req, res, next) => {
+  const id = req.params.voteId
+
+  res.status(200).json({
+    message: `Successfully deleted vote with ID: ${id}`
+  })
 })
 
 
--- vote-app-api/app.js
+++ vote-app-api/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser')
 const voteRoutes = require('./api/routes/votes')
 
 app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/vote', voteRoutes)
 
